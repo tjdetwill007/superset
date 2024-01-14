f422beb3a35896ecc999764ab25d988ee6bdac6b
Deepak <tjdetwill007@gmail.com>
2024-01-14 10:51:25 +0000

    Enabled the option of AWS S3

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -100,8 +100,7 @@ class CeleryConfig(object):
 
 CELERY_CONFIG = CeleryConfig
 
-FEATURE_FLAGS = {"ALERT_REPORTS": True}
-FEATURE_FLAGS = {"ENABLE_AWS": True}
+FEATURE_FLAGS = {"ALERT_REPORTS": True,"ENABLE_AWS": True}
 # ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
